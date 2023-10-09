beba5b99401039bf6d7e4436cfbad1ca792d9ce2
Zion Griffin <defendingmymaininbox@gmail.com>
2023-10-08 20:09:21 -0400

    Finished script.js so it will generate a password when prompted

--- script.js
+++ script.js
@@ -1,42 +1,64 @@
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // THEN I am presented with a series of prompts for password criteria
 // variables for length, uppercase, lowercase, numbers, special characters;
+
+//Initialize all variables
 var passLength; 
 var upperCase;
 var lowerCase;
 var passNumbers;
 var passSpecialCharacters;
 var charSet; 
+var lowerCaseSet = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseSet = lowerCaseSet.toUpperCase();
+var numbers = "1234567890";
+var specials = "!@#$%^&*()-+=[]{},.<>?/;:_'";
 
-// Create character set based on prompts (uppercase, lowercase, etc.)
 
+function generatePassword() {
+  var tempPassword = "";
+  for (var i = 0; i < passLength; i++) { 
+    tempPassword += charSet[Math.floor(Math.random() * charSet.length)]; //Chooses random character from charSet and adds it to tempPassword
+  }
+  return tempPassword;
+}
 
 // Write password to the #password input
 function writePassword() {
-  //var password = generatePassword();
+  
   var passwordText = document.querySelector("#password");
-  passLength = prompt("Enter password length from 8 to 128: ");
-  while (passLength > 128 || passLength < 8) {
-    passLength = prompt("Password not within defined parameters. Try again: ");
-  }
-  upperCase = confirm("Uppercase letters? ");
-  // WHEN prompted for password criteria
+  charSet = ""; //Re-initializes charSet when a new password is generated
   // THEN I select which criteria to include in the password
-  
-  
+  //Prompts user for inputs
+  passLength = prompt("Enter password length from 8 to 128: "); 
+  while (passLength > 128 || passLength < 8) { //Runs prompt again if passLength is greater than 128 or less than 8
+    passLength = prompt("Password not within listed parameters. Try again: ");
+  }
+  while (charSet === "") { //While loop makes the user redo prompts if all boolean variables are false
+    upperCase = confirm("Include uppercase letters?");
+    lowerCase = confirm("Include lowercase letters?");
+    passNumbers = confirm("Include numbers?");
+    passSpecialCharacters = confirm("Include special characters?");
+    // Create character set based on prompts (uppercase, lowercase, etc.)
+    if (lowerCase) {
+      charSet += lowerCaseSet;
+    }
+    if (upperCase) {
+      charSet += upperCaseSet;
+    }
+    if (passNumbers) {
+      charSet += numbers;
+    }
+    if (passSpecialCharacters) {
+      charSet += specials;
+    }
+    if (charSet === "") { //If all variables are false, then nothing will have been added to charSet
+      alert("Please select at least one.");
+    }
+  }
+  var password = generatePassword();
 
   passwordText.value = password;
 
